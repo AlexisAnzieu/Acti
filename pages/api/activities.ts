b777abea39174970bc49034fdd1ce21e75204390
alexis anzieu <alexis.anzieu@ssense.com>
2021-11-06 19:37:02 -0400

    edit api

--- pages/api/activities.ts
+++ pages/api/activities.ts
@@ -4,35 +4,34 @@ import { supabase } from '../../helpers/supabaseClient';
 import { definitions } from '../../type/supabase';
 
 type params = {
-  query: string;
-  id: string;
-  season: string;
+	query: string;
+	id: string;
+	season: string;
 }
 
 export default async function (
-  req: NextApiRequest,
-  res: NextApiResponse<any>
+	req: NextApiRequest,
+	res: NextApiResponse<any>
 ) {
-  const { query, id, season } = req.query as params;
-  let result;
-  let supabaseBase = supabase.from<definitions["activity"]>("activity").select("*");
-  if (id) {
-    result = await supabaseBase.eq('id', id).single();
-  } else if (query || season) {
-    if (query) {
-      supabaseBase.ilike("name", `%${query}%`)
-    }
-    if (season) {
-      supabaseBase.contains("seasons", [season])
-    }
-    result = await supabaseBase;
-    console.log(result)
-  } else {
-    result = await supabaseBase;
-  }
-  res.statusCode = result.status;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(result.data))
+	const { query, id, season } = req.query as params;
+	let result;
+	let supabaseBase = supabase.from<definitions["activity"]>("activity").select("*");
+	if (id) {
+		result = await supabaseBase.eq('id', id).single();
+	} else if (query || season) {
+		if (query) {
+			supabaseBase.ilike("name", `%${query}%`)
+		}
+		if (season) {
+			supabaseBase.contains("seasons", [season])
+		}
+		result = await supabaseBase;
+	} else {
+		result = await supabaseBase;
+	}
+	res.statusCode = result.status;
+	res.setHeader('Content-Type', 'application/json');
+	res.end(JSON.stringify(result.data))
 }
 
 
