fce3f52e8d83975d1f5d3705aafb86cbe46b22e6
alexis anzieu <alexis.anzieu@ssense.com>
2021-11-08 23:38:13 -0500

    remove crossorigin

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,8 +9,6 @@ export default class Document extends NextDocument {
                     <link
                         rel="stylesheet"
                         href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-                        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-                        crossorigin=""
                     />
                 </Head>
                 <body>
