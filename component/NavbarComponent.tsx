c15c02b702da5f5b87f30107e2d17c8337cdc1b1
alexis anzieu <alexis.anzieu@ssense.com>
2021-11-23 23:57:37 -0500

    ajout modal

--- component/NavbarComponent.tsx
+++ component/NavbarComponent.tsx
@@ -1,10 +1,18 @@
 import {
-    Box,
     Button,
     Center,
     Flex,
     Link as ChakraLink,
     Spacer,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Image,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -15,6 +23,7 @@ export type Locale = "en" | "fr";
 function Navbar() {
     const router = useRouter();
     const [locale, setLocale] = useState(router.locale as Locale);
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     const changeLocale = (value: Locale) => {
         setLocale(value);
@@ -47,7 +56,50 @@ function Navbar() {
                 </ChakraLink>
             </Center>
             <Spacer />
-            <Center w="150px">Qui somme nous?</Center>
+            <Center w="170px">
+                <Button onClick={onOpen} colorScheme="teal" variant="ghost">
+                    Qui sommes-nous?
+                </Button>
+
+                <Modal size="lg" isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Heureux de vous accueillir!</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Flex mb="10px">
+                                <Center w="50%">
+                                    <Image
+                                        borderRadius="full"
+                                        h="140px"
+                                        src="https://upduuutuduczuuhixpdp.supabase.in/storage/v1/object/public/profile/alexis.jpeg"
+                                        alt="Alexis Anzieu"
+                                    />
+                                </Center>
+                                <Center w="50%">
+                                    <Image
+                                        borderRadius="full"
+                                        h="140px"
+                                        src="https://upduuutuduczuuhixpdp.supabase.in/storage/v1/object/public/profile/carla.jpg"
+                                        alt="Carla Fabregas"
+                                    />
+                                </Center>
+                            </Flex>
+                            Nous sommes un jeune couple de deux ans (Alexis &
+                            Carla), arpentant le Québec à la recherche
+                            d'activités originales. <br /> Nos amis nous
+                            demandant regulièrement des retours d'expérience,
+                            l'idée nous est venue de déveloper un site web afin
+                            d'y capitaliser nos differentes trouvailles.
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button onClick={onClose} variant="ghost">
+                                Bonne visite!
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </Center>
             <Center cursor="pointer" mr="10px">
                 {(["en", "fr"] as Locale[]).map((language: Locale) => (
                     <Button
