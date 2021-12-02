c4c3fcc42ab8380785753c5a924d3044fc30288f
alexis anzieu <alexis.anzieu@ssense.com>
2021-12-02 12:11:55 -0500

    map go back to original fetch

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,33 +1,23 @@
 import { Box, Icon } from "@chakra-ui/react";
-import { GetStaticPropsContext } from "next";
+import { GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BsViewList } from "react-icons/bs";
 
 import { Locale } from "../component/NavbarComponent";
-import { searchApi } from ".";
+import { GetServerSideProps, searchApi } from ".";
 
-export default function Map() {
+export default function Map(props: GetServerSideProps["props"]) {
     const { t } = useTranslation("common");
     const router = useRouter();
     const MapWithNoSSR = dynamic(() => import("../component/MapComponent"), {
         ssr: false,
     });
-    const [locale] = useState(router.locale as Locale);
-    const [activities, setActivities] = useState([]);
-
-    useEffect(() => {
-        fetch(searchApi(router.query, locale))
-            .then((res: Response) => res.json())
-            .then((result) => {
-                setActivities(result);
-            });
-    }, []);
 
     return (
         <>
@@ -36,7 +26,7 @@ export default function Map() {
             </Head>
 
             <Box width="100%">
-                <MapWithNoSSR id="big-map" activities={activities} />
+                <MapWithNoSSR id="big-map" activities={props.activities} />
             </Box>
 
             <Box className="floating-button">
@@ -53,10 +43,20 @@ export default function Map() {
     );
 }
 
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale as Locale, ["common"])),
-        },
-    };
+export async function getServerSideProps(
+    context: GetServerSidePropsContext
+): Promise<GetServerSideProps> {
+    return fetch(searchApi(context.query, context.locale as Locale))
+        .then((res: Response) => res.json())
+        .then(async (activities) => {
+            return {
+                props: {
+                    ...(await serverSideTranslations(context.locale as Locale, [
+                        "common",
+                    ])),
+                    activities,
+                    queryParam: context.query,
+                },
+            };
+        });
 }
