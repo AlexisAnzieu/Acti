d2b5ce16426ba6c0f8f79438f8f7888be1f7a827
alexis anzieu <alexis.anzieu@ssense.com>
2021-12-09 23:48:12 -0500

    activities filter to print

--- pages/api/activities.ts
+++ pages/api/activities.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '../../helpers/supabaseClient';
 import { definitions } from '../../type/supabase';
 
-type params = {
+type Params = {
 	query: string;
 	id: string;
 	season: string;
@@ -16,26 +16,53 @@ export default async function (
 	req: NextApiRequest,
 	res: NextApiResponse<any>
 ) {
-	const { query, id, season, locale, fields = '*' } = req.query as params;
+	const { query, id, season, locale, fields = '*' } = req.query as Params;
+	const supabaseBase = supabase.from<definitions["activity"]>("activity").select(fields);
+
 	let result;
-	let supabaseBase = supabase.from<definitions["activity"]>("activity").select(fields).order('created_at', { ascending: false });
 	if (id) {
-		result = await supabaseBase.eq('id', id).single();
-	} else if (query || season) {
-		if (query) {
-			supabaseBase.ilike(`name->>${locale}` as 'name', `%${query}%`);
-		}
-		if (season) {
-			supabaseBase.contains("seasons", [season])
-		}
-		result = await supabaseBase;
+		result = await singleActivity(supabaseBase, { id });
+		return sendReponse(res, result);
+	}
+
+	if (query || season) {
+		result = await filterActivities(supabaseBase, { query, season, locale });
 	} else {
 		result = await supabaseBase;
 	}
 
+	result.data = result.data?.filter((activity: definitions["activity"]) => {
+		return activity.picture_url &&
+			activity.name &&
+			activity.description &&
+			activity.location &&
+			activity.seasons &&
+			activity.website
+	})
+	return sendReponse(res, result);
+}
+
+
+const singleActivity = async (supabaseBase: any, { id }: { id: Params['id'] }) => supabaseBase.eq('id', id).single();
+
+const filterActivities = async (
+	supabaseBase: any, { query, season, locale }: {
+		query: Params['query'],
+		season: Params['season'],
+		locale: Params['locale']
+	}) => {
+	if (query) {
+		supabaseBase.ilike(`name->>${locale}` as 'name', `%${query}%`);
+	}
+	if (season) {
+		supabaseBase.contains("seasons", [season])
+	}
+	return supabaseBase;
+};
+
+
+const sendReponse = (res: NextApiResponse<any>, result: any) => {
 	res.statusCode = result.status;
 	res.setHeader('Content-Type', 'application/json');
 	res.end(JSON.stringify(result.data))
 }
-
-
