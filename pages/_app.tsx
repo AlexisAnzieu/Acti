65581641f1b55186595787ad3e90d26caadcbacb
alexis anzieu <alexis.anzieu@ssense.com>
2021-12-14 00:24:49 -0500

    lint fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import "../styles/globals.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { appWithTranslation } from "next-i18next";
 import Script from "next/script";
+import { appWithTranslation } from "next-i18next";
 
 import Navbar from "../component/NavbarComponent";
 
