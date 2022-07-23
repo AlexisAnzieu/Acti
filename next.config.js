059420f1d67151bdc3abd08f85b547ffb44d2353
alexis anzieu <alexis.anzieu@ssense.com>
2022-07-23 12:37:14 +0200

    add new photo config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
   reactStrictMode: true,
   i18n,
   images: {
-    domains: ['xyzfqjxmadywqhwvcqxh.supabase.in', 'picsum.photos'],
+    domains: ['xyzfqjxmadywqhwvcqxh.supabase.in', 'picsum.photos', 'acti.anzieu.fr'],
   },
 }
