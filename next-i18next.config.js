850d543cf0056d725bd38cc5d76d7a20e9fde31e
alexis anzieu <alexis.anzieu@ssense.com>
2023-07-16 18:00:12 -0400

    maybe

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
     i18n: {
         locales: ["default", "en", "fr"],
         defaultLocale: "default",
         localeDetection: false,
+        localePath: path.resolve("./public/locales"),
     },
 };
