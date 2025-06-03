17683e2bead938e80886c5964f23462554fc0fcc
Alexis anzieu <alexis.anzieu@ssense.com>
2025-06-03 16:33:00 -0400

    feat(transcanadian): update i18n configuration by removing default locale and associated files for improved localization

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 module.exports = {
     i18n: {
-        locales: ["default", "en", "fr"],
-        defaultLocale: "default",
+        locales: ["en", "fr"],
+        defaultLocale: "en",
         localeDetection: false,
         localePath: path.resolve("./public/locales"),
     },
